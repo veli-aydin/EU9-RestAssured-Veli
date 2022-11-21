b1d074b150064f898924d854fb43a82329d30107
veli-aydin <veli-aydin@hotmail.com>
2022-11-21 10:38:05 +0300

    4th commit

--- src/test/java/com/cydeo/day2/SpartanGetRequests.java
+++ src/test/java/com/cydeo/day2/SpartanGetRequests.java
@@ -3,7 +3,6 @@ package com.cydeo.day2;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
